68d343d7b850907688bf3e11d562a2d2c26e644b
dustin-ward <wardddustin@gmail.com>
2021-07-21 17:37:27 -0600

    Args

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip3 install -r requirements.txt
+sudo apt-get install tesseract-ocr
+sudo apt-get install poppler-utils
--- pdf-ocr.py
+++ pdf-ocr.py
@@ -4,18 +4,31 @@ import sys
 import os
 from pdf2image import convert_from_path
 
-file = "document.pdf"
+file = sys.argv[1]
+keyword = sys.argv[3]
 pages = convert_from_path(file, 500)
 
 for i, page in enumerate(pages):
     page.save("images/page" + str(i) + ".jpg", 'JPEG')
 
-output_file = "document_text.txt"
+output_file = sys.argv[2]
 f = open(output_file, "a")
 
+print("Instances of key:", keyword)
 for i in range(0, len(pages)):
     text = str(((pytesseract.image_to_string(Image.open("images/page" + str(i) + ".jpg")))))
     text = text.replace('-\n', '') 
+
+    idx = 0
+    while True:
+        idx = text.find(keyword, idx)
+        if idx == -1:
+            break
+        
+        print("PAGE:", i+1, str(round(idx/len(text)*100, 2)), "%")
+
+        idx += 1
+
     f.write(text)
 
 f.close()
