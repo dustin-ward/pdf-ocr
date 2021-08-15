5fdb78553c5830c33d50f7ec635570560693a740
dustin-ward <wardddustin@gmail.com>
2021-08-15 16:19:33 -0600

    Multiple keywords, and cleaning

--- pdf-ocr.py
+++ pdf-ocr.py
@@ -2,33 +2,71 @@ from PIL import Image
 import pytesseract
 import sys
 import os
+import shutil
 from pdf2image import convert_from_path
 
+# Cmd line args
 file = sys.argv[1]
-keyword = sys.argv[3]
-pages = convert_from_path(file, 500)
+output_file = sys.argv[2]
+keywords = sys.argv[3:]
 
+# Check for and create img directory
+imgDirectory = os.getcwd() + "/images"
+try:
+    os.stat(imgDirectory)
+except:
+    os.mkdir(imgDirectory)   
+
+# Convert PDF pages to JPG images
+pages = convert_from_path(file, 500)
 for i, page in enumerate(pages):
     page.save("images/page" + str(i) + ".jpg", 'JPEG')
 
-output_file = sys.argv[2]
-f = open(output_file, "a")
+# Gather keywords
+instances = {}
+for k in keywords:
+    instances[k] = []
 
-print("Instances of key:", keyword)
+# For each page...
+f = open(output_file, "a")
 for i in range(0, len(pages)):
+    # Convert JPG to txt
     text = str(((pytesseract.image_to_string(Image.open("images/page" + str(i) + ".jpg")))))
     text = text.replace('-\n', '') 
 
-    idx = 0
-    while True:
-        idx = text.find(keyword, idx)
-        if idx == -1:
-            break
-        
-        print("PAGE:", i+1, str(round(idx/len(text)*100, 2)), "%")
+    # For each keyword...
+    for k in keywords:
+        idx = 0
+        while True:
+            idx = text.find(k, idx)
+            if idx == -1:
+                break
+            
+            # Add page # and position to dictionary
+            instances[k].append((i+1, round(idx/len(text)*100, 2)))
 
-        idx += 1
+            idx += 1
 
     f.write(text)
 
+# Print results
+for k, l in instances.items():
+    print("INSTANCES OF KEYWORD", k, ":")
+    if len(l):
+        for i in l:
+            print("PAGE:", i[0], i[1], "%")
+    else:
+        print("None")
+
 f.close()
+
+# Clean img directory
+for filename in os.listdir(imgDirectory):
+    file_path = os.path.join(imgDirectory, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
